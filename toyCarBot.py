0ff21344251dcb38b8a915ba119f87392e64baa2
rrich922 <rrich922@gmail.com>
2022-08-24 14:20:47 +0800

    use waitress

--- toyCarBot.py
+++ toyCarBot.py
@@ -192,5 +192,6 @@ def handle_postback(event):
             TextSendMessage(text=action))    
 
 if __name__ == '__main__':
-
-    app.run(port=5002, debug=False)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5002)
+    #app.run(port=5002, debug=False)
