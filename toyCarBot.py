bbf19d54b6093ec7df2611923ebb57dfbc6646fd
rrich922 <rrich922@gmail.com>
2022-10-04 13:58:49 +0800

    fix

--- toyCarBot.py
+++ toyCarBot.py
@@ -110,7 +110,7 @@ def handle_message(event):
     line_bot_api.reply_message(
          event.reply_token,
          (TextSendMessage(text=msg),
-          TextSendMessage(text=intro))
+          intro)
           )
     
 
